17baa8d3818459d73e8bf19b54be98845386e939
Yonier Asprilla <samirzapata11@gmail.com>
2022-04-08 02:31:48 -0500

    Conectado a la BD alojada en firebase, diseño de login ok, diseño start ok, trabajando en diseño singUp

--- app/src/main/java/com/usbbog/edu/parkingmusb/view/login.java
+++ app/src/main/java/com/usbbog/edu/parkingmusb/view/login.java
@@ -2,13 +2,23 @@ package com.usbbog.edu.parkingmusb.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class login extends AppCompatActivity {
+
+    TextView btnSingUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+    }
+    public void onClick(View view) {
+        Intent i = new Intent(login.this,singUp.class);
+        startActivity(i);
     }
 }
