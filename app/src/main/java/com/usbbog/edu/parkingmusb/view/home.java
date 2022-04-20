5e8a18ed56b1454e9db0b03b1322eea90b6bfd51
Yonier Asprilla <samirzapata11@gmail.com>
2022-04-20 01:54:46 -0500

    Conectado a la BD alojada en firebase, diseño de login ok, diseño start ok, trabajando en diseño singUp

--- app/src/main/java/com/usbbog/edu/parkingmusb/view/home.java
+++ app/src/main/java/com/usbbog/edu/parkingmusb/view/home.java
@@ -1,14 +1,59 @@
 package com.usbbog.edu.parkingmusb.view;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class home extends AppCompatActivity {
+
+    HomeFragment home = new HomeFragment();
+    parkFragment park = new parkFragment();
+    newsFragment news = new newsFragment();
+    profileFragment profile = new profileFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        BottomNavigationView navigation = findViewById(R.id.nav_abajo);
+        navigation.setOnNavigationItemSelectedListener((BottomNavigationView.OnNavigationItemSelectedListener) mOnNavigationItemSelectedListener);
+
+        loadFragment(home);
     }
+
+    private final BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()){
+                case R.id.home:
+                    loadFragment(home);
+                    return true;
+                case R.id.park:
+                    loadFragment(park);
+                    return true;
+                case R.id.news:
+                    loadFragment(news);
+                    return true;
+                case R.id.profile:
+                    loadFragment(profile);
+                    return true;
+            }
+            return false;
+        }
+
+    };
+
+    public void loadFragment(Fragment fragment){
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_container, fragment);
+        transaction.commit();
+    }
+
 }
