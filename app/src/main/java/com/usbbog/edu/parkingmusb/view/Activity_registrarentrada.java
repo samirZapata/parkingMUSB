219c702a5b4ac4a1adba9fd9ca24726d7340d03d
Juancuervoh <juancuervoh@hotmail.com>
2022-05-09 12:44:31 -0500

    Interfaz vigilante: principal y registro

---
+++ app/src/main/java/com/usbbog/edu/parkingmusb/view/Activity_registrarentrada.java
@@ -0,0 +1,14 @@
+package com.usbbog.edu.parkingmusb.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_registrarentrada extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrarentrada);
+    }
+}
---
+++ app/src/main/java/com/usbbog/edu/parkingmusb/view/activity_mainVigil.java
@@ -0,0 +1,14 @@
+package com.usbbog.edu.parkingmusb.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_mainVigil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_vigil);
+    }
+}
